96bd2401cc12381f8240c44b16e1458a0410ed08
Eklavya47 <eklavya5924@gmail.com>
2025-11-16 20:26:57 +0530

    Initial Commit

---
+++ app/src/main/java/com/betanooblabs/rockpaperscissor/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.betanooblabs.rockpaperscissor
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val btnPlayWithOther: Button = findViewById(R.id.btnPlayWithOther)
+        btnPlayWithOther.setOnClickListener {
+
+        }
+
+        val btnPlayWithComputer: Button = findViewById(R.id.btnPlayWithComputer)
+        btnPlayWithComputer.setOnClickListener {
+
+        }
+
+        val tvInstruction: TextView = findViewById(R.id.tvInstruction)
+        tvInstruction.setOnClickListener {
+
+        }
+    }
+}
